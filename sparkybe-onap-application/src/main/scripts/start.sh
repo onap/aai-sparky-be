959cdac3aa172a62a7674a3995d61753a4eb842b
da490c <dave.adams@amdocs.com>
2018-04-04 17:12:21 -0400

    Add support for sparky logback xml config
    Issue-ID: AAI-988
    Change-Id: I22b46b559534cb749b5ba402c229e55fbcb2518b
    Signed-off-by: da490c <dave.adams@amdocs.com>

--- sparkybe-onap-application/src/main/scripts/start.sh
+++ sparkybe-onap-application/src/main/scripts/start.sh
@@ -3,7 +3,7 @@
 APP_HOME="/opt/app/sparky"
 CONFIG_HOME=${APP_HOME}/appconfig
 
-PROPS="-DAPP_HOME=${APP_HOME} -DCONFIG_HOME=${CONFIG_HOME}"
+PROPS="-DAPP_HOME=${APP_HOME} -DCONFIG_HOME=${CONFIG_HOME} -Dlogging.config=${CONFIG_HOME}/logging/logback.xml"
 
 set -x
 jar ufv ${APP_HOME}/lib/sparkybe-onap-application*.jar -C ${CONFIG_HOME}/portal/ BOOT-INF/classes/portal.properties
