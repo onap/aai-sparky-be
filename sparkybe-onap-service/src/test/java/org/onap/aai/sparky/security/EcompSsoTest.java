9b33f2521b15c3d124ac3299fcd6d61081954b5d
Driptaroop Das <driptaroop.das@in.ibm.com>
2019-01-16 16:23:27 +0530

    Junit for EcompSso
    Junit for EcompSso
    
    Issue-ID: AAI-2080
    Change-Id: I69e787dba2ab5ceb6d1be0820dd847dc8aa69a81
    Signed-off-by: Driptaroop Das <driptaroop.das@in.ibm.com>

--- sparkybe-onap-service/src/test/java/org/onap/aai/sparky/security/EcompSsoTest.java
+++ sparkybe-onap-service/src/test/java/org/onap/aai/sparky/security/EcompSsoTest.java
@@ -1,8 +1,26 @@
+/**
+ * ============LICENSE_START=======================================================
+ * org.onap.aai
+ * ================================================================================
+ * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright © 2017-2018 Amdocs
+ * Copyright © 2019 IBM
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
 package org.onap.aai.sparky.security;
-
-import static org.junit.Assert.assertNotNull;
-
-import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +28,12 @@ import org.mockito.Mockito;
 import org.onap.aai.sparky.security.portal.PortalRestAPICentralServiceImpl;
 import org.onap.portalsdk.core.onboarding.exception.PortalAPIException;
 import org.onap.portalsdk.core.restful.domain.EcompRole;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 
 
 public class EcompSsoTest {
@@ -30,6 +54,22 @@ public class EcompSsoTest {
     Mockito.when(portalRestCentralServiceImpl.getUserRoles(Mockito.anyString()))
         .thenReturn(new ArrayList<EcompRole>());
     assertNotNull(ecompSso.validateUserAccess("ui_view"));
+  }
+
+  @Test
+  public void getCookie() {
+    String TestCookieValue = "TestCookieValue";
+    String testCookieName = "testCookieName";
+    Cookie cookie1 = new Cookie(testCookieName+"1", TestCookieValue+"1");
+    Cookie cookie2 = new Cookie(testCookieName+"2", TestCookieValue+"2");
+    Cookie[] cookies = {cookie1, cookie2};
+
+    HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+    Mockito.when(request.getCookies()).thenReturn(cookies);
+    Cookie result = EcompSso.getCookie(request, testCookieName + "1");
+    assertEquals(cookie1.getName(), result.getName());
+    assertEquals(cookie1.getValue(), result.getValue());
+    assertNull(EcompSso.getCookie(request, testCookieName + "3"));
   }
 
 
