bd0e9cbc6363c27e3126ddd163e7b7c379ebedeb
Dmitry Puzikov <dmitry.puzikov@tieto.com>
2019-07-19 14:52:52 +0200

    Fixed Dockerfile to reflect new base image naming
    FROM section of Dockerfile has been changed
    to follow new aai-common image naming scheme.
    
    Added maven parameter for aai-common version.
    
    Change-Id: Iedc6b809dcc4f81c6d5e2424432b9adeadcd3941
    Issue-ID: INT-1150
    Signed-off-by: Dmitry Puzikov <dmitry.puzikov@tieto.com>

--- sparkybe-onap-application/src/main/scripts/start.sh
+++ sparkybe-onap-application/src/main/scripts/start.sh
@@ -6,7 +6,9 @@ CONFIG_HOME=${APP_HOME}/config
 PROPS="-DAPP_HOME=${APP_HOME} -DCONFIG_HOME=${CONFIG_HOME} -Dlogging.config=${CONFIG_HOME}/logging/logback.xml"
 
 set -x
-jar ufv ${APP_HOME}/lib/sparkybe-onap-application*.jar -C ${CONFIG_HOME}/portal/ BOOT-INF/classes/portal.properties -C ${CONFIG_HOME}/portal/ BOOT-INF/classes/key.properties
+jar ufv ${APP_HOME}/lib/sparkybe-onap-application*.jar \
+    -C ${CONFIG_HOME}/portal/ BOOT-INF/classes/portal.properties \
+    -C ${CONFIG_HOME}/portal/ BOOT-INF/classes/key.properties > /dev/null 2>&1
 
 #
 # change the working directory so that Spring-Boot will pick up the config folder from the right path
